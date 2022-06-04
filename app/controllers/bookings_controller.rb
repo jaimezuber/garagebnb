d5822deb8579c2b588388b9edd50ba10dc8664de
jaime zuber <jaimezuber99@gmail.com>
2022-06-04 18:08:40 -0300

    Controller basico de garages terminado

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,2 +1,30 @@
 class BookingsController < ApplicationController
+
+  def index
+
+  end
+
+  def show
+    @booking = Booking.find(params[:id])
+  end
+
+  def new
+    @booking = Booking.new
+  end
+
+  def create
+
+  end
+
+  def edit
+
+  end
+
+  def update
+
+  end
+
+  def delete
+
+  end
 end
---
+++ test/controllers/garages_controller_test.rb
@@ -0,0 +1,38 @@
+require "test_helper"
+
+class GaragesControllerTest < ActionDispatch::IntegrationTest
+  test "should get index" do
+    get garages_index_url
+    assert_response :success
+  end
+
+  test "should get show" do
+    get garages_show_url
+    assert_response :success
+  end
+
+  test "should get new" do
+    get garages_new_url
+    assert_response :success
+  end
+
+  test "should get create" do
+    get garages_create_url
+    assert_response :success
+  end
+
+  test "should get edit" do
+    get garages_edit_url
+    assert_response :success
+  end
+
+  test "should get update" do
+    get garages_update_url
+    assert_response :success
+  end
+
+  test "should get delete" do
+    get garages_delete_url
+    assert_response :success
+  end
+end
