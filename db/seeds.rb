28c326a708602f11df8c38467984a3631a1a5558
jaime zuber <jaimezuber99@gmail.com>
2022-06-07 22:57:36 -0300

    le falta :photo y hacer la migracion agregando price y title

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+usuario1 = User.new(full_name: 'joel', email: 'joel@gmail.com', password: 'joel')
+if usuario1.save
+  puts "joel created"
+else
+  puts "joel failed"
+end
+usuario2 = User.new(full_name: 'maxi', email: 'maxi@gmail.com', password: 'maxi')
+if usuario2.save
+  puts "maxi created"
+else
+  puts "maxi failed"
+end
+usuario3 = User.new(full_name: 'jaime', email: 'jaime@gmail.com', password: 'jaime')
+if usuario3.save
+  puts "jaime created"
+else
+  puts "jaime failed"
+end
+usuario4 = User.new(full_name: 'luca', email: 'luca@gmail.com', password: 'luca')
+if usuario4.save
+  puts "luca created"
+else
+  puts "luca failed"
+end
+Garage.create(owner: usuario1, location:'Garage de joel', photo:, price: 10)
+Garage.create(owner: usuario2, location:'Garage de maxi', photo:, price: 8)
+Garage.create(owner: usuario3, location:'Garage de jaime', photo:,price: 25)
+Garage.create(owner: usuario4, location:'Garage de luca', photo:, price: 40)
