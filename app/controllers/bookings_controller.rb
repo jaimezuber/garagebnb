848a9404133653f716b0a29f447ed363a0ea2d7f
jaime zuber <jaimezuber99@gmail.com>
2022-06-09 16:07:02 -0300

    routes y controller de bookings creados

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -1,29 +1,51 @@
 class BookingsController < ApplicationController
-  def index
 
-  end
-
-  def show
-    @booking = Booking.find(params[:id])
-  end
+  before_action :set_booking, only: [:edit, :update, :destroy, :accept_booking]
 
   def new
     @booking = Booking.new
+    @garage = Garage.find[params[:garage_id]]
   end
 
   def create
-
+    @booking = Booking.new(rev_params)
+    @garage = Garage.find[params[:garage_id]]
+    @client = current_user
+    @booking.garage = @garage
+    @booking.client = @client
+    if @booking.save
+      redirect_to garage_path(@garage)
+    else
+      render :new
+    end
   end
 
-  def edit
-
-  end
+  def edit; end
 
   def update
-
+    if @booking.update(rev_params)
+      redirect_to garage_path(@booking.garage)
+    else
+      render :edit
+    end
   end
 
-  def delete
+  def destroy
+    @booking.destroy
+    redirect_to garage_path(@booking.garage)
+  end
 
+  def accept_booking
+    @booking.status = 'accepted'
+  end
+
+  private
+
+  def rev_params
+    params.require(:booking).permit(:initial_date, :end_date)
+  end
+
+  def set_booking
+    Booking.find(params[:id])
   end
 end
