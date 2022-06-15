fd74123e4085dac9523f33a713fdccfe43e1114d
jaime zuber <jaimezuber99@gmail.com>
2022-06-15 15:36:26 -0300

    scope para el metodo confirmed en bookings

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,4 +2,6 @@ class Booking < ApplicationRecord
   belongs_to :client, class_name: "User", foreign_key: "client_id"
   belongs_to :garage
   has_many :reviews
+
+  scope :confirmed, -> { where(status: "confirmed") }
 end
